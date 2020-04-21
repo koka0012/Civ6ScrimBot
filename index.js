a3cf5fac6bd1b582fc9dea481ff4704d4ea0011e
York <YorkAARGH@users.noreply.github.com>
2020-04-22 00:57:51 +0100

    Quick Fix
    Fixed a bug I didn't realise we had,

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ class GuideBot extends Client {
   // getSettings merges the client defaults with the guild settings. guild settings in
   // enmap should only have *unique* overrides that are different from defaults.
   getSettings (guild) {
-    const defaults = this.config.defaultSettings || {};
+    const defaults = this.settings.get("default") || {};
     const guildData = this.settings.get(guild.id) || {};
     const returnObject = {};
     Object.keys(defaults).forEach((key) => {
