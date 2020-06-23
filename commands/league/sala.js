1b300e10aa3b2fd55a334849b402405271b9839c
Augusto Chmieleski <augustocezarchmieleski@gmail.com>
2020-06-22 22:20:21 -0300

    .

--- commands/league/sala.js
+++ commands/league/sala.js
@@ -34,8 +34,18 @@ class Sala extends Command {
     const players = await Player.find({discordId: {$in: membersId}});
     let msg = '';
     players.forEach(_ => {
+      membersId.slice(membersId.indexOf(_.id), 1);
       msg+= `${this.spaceAdd(message.guild.member(_.discordId).displayName, 30)}${this.spaceAdd(_.rating.toFixed(0), 10)}${message.guild.member(_.discordId).roles.cache.find(r => roles.includes(r.name)).name || 'Colono'}\n`;
     });
+
+    const settings = this.client.getSettings(message.guild);
+
+    if (membersId.length > 0) {
+      membersId.forEach(_ => {
+        msg+= `${this.spaceAdd(message.guild.member(_.discordId).displayName, 30)}${this.spaceAdd(settings.defaultRating, 10)}${message.guild.member(_.discordId).roles.cache.find(r => roles.includes(r.name)).name || 'Colono'}\n`;
+      });
+    }
+
     await message.channel.send(msg,{code:'asciidoc'});
   }
 
